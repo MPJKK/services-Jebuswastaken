e136c672292aafbdf6f966b5f8390b33f3e9ce1b
Markus Vikholm <markuvik@ety-a2123b-13.edu.wks.metropolia.fi>
2018-01-25 10:40:04 +0200

    awawawweewa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ListMediaComponent } from './list-media/list-media.component';
+import {MediaService} from './services/media.service';
+import {HttpClientModule} from '@angular/common/http';
+import {DigitransitService} from './services/digitransit.service';
+import { RoutesComponent } from './routes/routes.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ListMediaComponent,
+        RoutesComponent
+    ],
+    imports: [
+        BrowserModule, HttpClientModule
+    ],
+    providers: [MediaService, DigitransitService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
